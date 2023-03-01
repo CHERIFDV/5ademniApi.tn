09f2fe79893e7e7bdccf0748befa0786fccf631b
GUESAIER Cherif <cherifgsair@gmail.com>
2023-03-01 23:58:50 +0100

    after_run_hashing_password

---
+++ src/Controller/UserController.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+/*/*/
+
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+/**
+ * @Route("/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_user_index", methods={"GET"})
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/getall", name="app_userss_getall", methods={"GET"})
+     */
+    public function getall(UserRepository $userRepository,SerializerInterface $serializer): JsonResponse
+    {
+       
+      $votes=$userRepository->findAll();
+     
+      $jsonBookList = $serializer->serialize($votes, 'json');
+      return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true); 
+        
+    }
+
+    /**
+     * @Route("/new", name="app_user_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+     /**
+     * @Route("/new_user_api", name="app_user_new_user_api", methods={"GET", "POST"})
+     */
+    public function newuserapi(Request $request, UserRepository $userRepository, ManagerRegistry $doctrine,SerializerInterface $serializer,UserPasswordHasherInterface $passwordHasher ):JsonResponse
+    {
+      
+        $em = $doctrine->getManager();
+      
+      
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        /*/$user= new User();
+        $user->setEmail("cherif@gmail.com");
+        $user->setRoles(["cvbcvb"]);
+
+        $user->setPassword("vcbcvb");
+
+        $user->setFirstName("cvbcvb");
+
+        $user->setLastName("vbcvb");
+
+        $user->setTel("1542453");
+
+        $user->setBio("gbfcgfd");
+
+        $user->setTypeProfile(true);/*/
+      
+
+
+
+
+
+
+
+        $plaintextPassword = "fdgdfg";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $em->persist($user);
+        $em->flush();
+
+        $jsonBook = $serializer->serialize($user, 'json', ['groups' => 'getBooks']);
+        
+       
+
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, [], true);
+    }
+
+    /**
+     * @Route("/{id}", name="app_user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_user_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_user_delete", methods={"POST"})
+     */
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
